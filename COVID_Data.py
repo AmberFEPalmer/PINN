bced40d5ea645702832ae432008558573d64ec0f
AmberFEPalmer <amberfe.palmer@bristol.ac.uk>
2025-12-03 17:17:14 +0000

    changed case data to prevelance data

--- COVID_Data.py
+++ COVID_Data.py
@@ -8,55 +8,48 @@ import matplotlib.pyplot as plt
 data = pd.read_csv("/Users/oc25003/Desktop/Data/Cases/nation_newCasesBySpecimenDate.csv")
 ### Selecting only the date and the number of positive cases from the dataset
 data = data[['date', 'value']].rename(columns={'value': 'newCases'})
-### Convert date column to date time
+### Convert date column to date time format
 data['date'] = pd.to_datetime(data['date'])
 
-### 7 day average of cases - consistent with Li et al. 2024 Journal of medical bioinformatics
-data['newCases_smooth'] = data['newCases'].rolling(window=7, center=True, min_periods=1).mean()
+### Smooth 7-day average - consistent with published literature e.g. Li et al. 2024
+data['newCases_smooth'] = data['newCases'].rolling(7, center=True, min_periods=1).mean()
 
-### Convert date to time variable
+### Filter time window
+### These dates show increasing cases therefore easy start for PINN
+### (mask = filter)
+start_date = '2021-12-15'
+end_date   = '2021-12-31'
+mask = (data['date'] >= start_date) & (data['date'] <= end_date)
+data = data[mask]
+
+### Normalized time to [0,1]
+### (make time points between 0 and 1 - PINN works better with values between 0 and 1)
 data['t'] = (data['date'] - data['date'].min()).dt.days
-t_min, t_max = data['t'].min(), data['t'].max()
-data['t_norm'] = (data['t'] - t_min) / (t_max - t_min)
+data['t_norm'] = (data['t'] - data['t'].min()) / (data['t'].max() - data['t'].min())
 
-### N = Population of UK
+### Convert daily cases to infectious prevalence
+### raw data measures new infections but number of currently infected people is needed for the SEIR model
+### assume each individual remains infected for 5 days
+infectious_period = 5
+data['I_prev'] = data['newCases_smooth'].rolling(
+    infectious_period, min_periods=1
+).sum()
+
+### Convert infection prevelance into a fraction of the population of the UK
 N = 69_000_000
-### Convert absolute daily new cases into the proportion of the population
-data['I_obs'] = data['newCases_smooth'] / N
+data['I_obs'] = data['I_prev'] / N
 
-### Save data
-### NumPy arrays
-np.save("t_data.npy", data['t_norm'].values.reshape(-1, 1))
-np.save("I_data.npy", data['I_obs'].values.reshape(-1, 1))
+### Save arrays for PINN (neural networks need arrays rather than dataframes)
+np.save("t_data.npy",  data['t_norm'].values.reshape(-1, 1))
+np.save("I_data.npy",  data['I_obs'].values.reshape(-1, 1))
+np.save("t_col.npy", np.random.uniform(0, 1, (2000, 1)))
 
-### Generate collocation points for PINN physics loss
-### (Collocation points constitute the training dataset for PINN)
-n_col = 2000
-t_col = np.random.uniform(0, 1, (n_col, 1))
-np.save("t_col.npy", t_col)
-
-### Data visualisation
-
-### Dates of COVID-19 government restrictions (UK)
-lockdowns = [
-    ("Lockdown 1", "2020-03-23"),
-    ("Lockdown 2", "2020-12-02"),
-    ("Lockdown 3", "2021-01-04")
-]
-
+## Visualization
 plt.figure(figsize=(12, 5))
-plt.plot(data['date'], data['I_obs'] * N, label="Observed infected (proxy)", color='pink')
-plt.title("COVID-19: Observed Infected Proxy")
+plt.plot(data['date'], data['I_obs'] * N, label="Estimated infectious prevalence", color='blue')
+plt.scatter(data['date'], data['newCases'], label="Daily new cases", color='pink', alpha=0.5)
+plt.title("COVID-19: Estimated I(t) for SEIR PINN")
 plt.ylabel("People")
-plt.xlabel("Date")
 plt.grid(True)
-
-for label, date in lockdowns:
-    plt.axvline(pd.to_datetime(date), color='grey', linestyle='--', linewidth=1)
-    plt.text(pd.to_datetime(date), plt.ylim()[1], label,
-             rotation=90, verticalalignment='top', color='grey')
-
 plt.legend()
 plt.show()
-
-
